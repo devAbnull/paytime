ad795dc49f972faee3b71f4ac639b3dace6aad50
rwobben <r.wobben@home.nl>
2016-01-03 19:18:53 +0000

    make the first functions

--- paytime/src/Lib.hs
+++ paytime/src/Lib.hs
@@ -1,6 +1,21 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Amount = Integer
+type Account = Integer
+type Description = String
+
+data Transaction = Transaction Amount Description
+
+applyTransaction :: Transaction -> Account -> Account
+applyTransaction (Transaction amount _) account = account + amount
+
+deposit :: Amount ->  Description -> Account -> Account
+deposit amount description account = applyTransaction (Transaction amount description) account
+
+withdraw :: Amount -> Description -> Account -> Account
+withdraw amount description account = applyTransaction (Transaction (- amount ) description) account
+
+applyTransactions :: [Transaction] -> Account -> Account
+applyTransactions [] account = account 
+applyTransactions (x:xs) acc = applyTransactions xs newAcccount
+  where newAcccount = (applyTransaction x acc)
