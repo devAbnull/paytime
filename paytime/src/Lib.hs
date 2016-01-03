da3293a36f769c129c952a5cdd82f6e23f7ce861
rwobben <r.wobben@home.nl>
2016-01-03 19:28:05 +0000

    hopefully solved it now

--- paytime/src/Lib.hs
+++ paytime/src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Amount = Integer
+type Account = Integer
+type Description = String
+
+data Transaction = Transaction { transactionAmount :: Amount
+                               , transactionDescription :: Description}
+                               
+applyTransaction :: Transaction -> Account -> Account
+applyTransaction (Transaction amount _) account = account + amount
+
+deposit :: Amount ->  Description -> Account -> Account
+deposit amount description = applyTransaction (Transaction amount description)
+
+withdraw :: Amount -> Description -> Account -> Account
+withdraw amount description  = applyTransaction (Transaction (- amount ) description)
+
+applyTransactions :: [Transaction] -> Account -> Account
+applyTransactions [] account = account 
+applyTransactions (x:xs) acc = applyTransactions xs newAcccount
+  where newAcccount = applyTransaction x acc
